f4ab86d720d766b465fece04ed8af4751a115865
igorsid <sidgarri@gmail.com>
2022-07-27 01:46:08 +0800

    exp tests

---
+++ src/test/java/tests/CheckScreenTests.java
@@ -0,0 +1,42 @@
+package tests;
+
+import io.appium.java_client.AppiumBy;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+@Tag("android")
+public class CheckScreenTests extends TestBase {
+
+    @Test
+    @AllureId("11594")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    @Tag("demowebshop")
+    @Story("Login tests")
+    @Owner("allure8")
+    void checkOnboardingScreen() {
+        step("Check first screen", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text("The Free Encyclopedia …in over 300 languages"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        });
+        step("Check second screen", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text("New ways to explore"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        });
+        step("Check third screen", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text("Reading lists with sync"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
+        });
+        step("Check last screen", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text("Send anonymous data"));
+        });
+    }
+}
+
---
+++ src/test/java/tests/OpenPageTests.java
@@ -0,0 +1,47 @@
+package tests;
+
+import io.appium.java_client.AppiumBy;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+
+public class OpenPageTests extends TestBase {
+    @Test
+    @AllureId("11593")
+    @DisplayName("Check page")
+    @Tag("demowebshop")
+    @Story("Login tests")
+    void openFourPagesAndCheck() {
+        step("Check first page", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                        .shouldHave(text("The Free Encyclopedia")));
+
+        step("Open second page", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Check second page", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                        .shouldHave(text("New ways to explore")));
+
+        step("Open third page", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Check third page", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                        .shouldHave(text("Reading lists with sync")));
+
+        step("Open fourth page", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Check fourth page", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                        .shouldHave(text("Send anonymous data")));
+
+    }
+}
