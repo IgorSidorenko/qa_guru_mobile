150785468b72502a0b4b92dd1d861ae2e81405b2
igorsid <sidgarri@gmail.com>
2022-07-28 22:13:35 +0800

    format code

--- src/test/java/config/BrowserstackConfig.java
+++ src/test/java/config/BrowserstackConfig.java
@@ -1,22 +1,5 @@
 package config;
 
-/*import org.aeonbits.owner.Config;
-
-@Config.Sources("classpath:config/credentials.properties")
-public interface BrowserstackConfig extends Config {
-    @Key("user")
-    String userLogin();
-    @Key("key")
-    String userPassword();
-    String appURL();
-    String device();
-    String osVersion();
-    String project();
-    String build();
-    String name();
-    String browserURL();
-    String appPath();
-}*/
 import org.aeonbits.owner.Config;
 
 @Config.Sources("classpath:config/credentials.properties")
